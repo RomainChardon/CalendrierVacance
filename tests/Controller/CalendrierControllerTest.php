ebf3927f91ec4e4ebceb15433cebef69b5d96b99
RomainChardon <romain.chardon.sio2020@gmail.com>
2022-01-14 16:40:08 +0100

    testunit

--- tests/Controller/CalendrierControllerTest.php
+++ tests/Controller/CalendrierControllerTest.php
@@ -4,9 +4,10 @@ namespace App\Tests\Controller;
 
 use App\Entity\Groupe;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -21,33 +22,37 @@ class CalendrierControllerTest extends WebTestCase
         $this->assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
     }
 
-    // $crawler = $client->request('GET', '/');
-        // $csrfToken = $client->getContainer()->get("security.csrf.token_manager")->getToken('authenticate');
-        // $form = $crawler->selectButton('Connexion')->form([
-        //     'username' => 'rchardon',
-        //     'password' => 'toto',
-        //     'csrf_token' => $csrfToken,
-        // ]);
-        // $client->submit($form);
-        // $this->assertResponseStatusCodeSame(302);
-        
-        // $client->followRedirect();
-        // $this->assertResponseStatusCodeSame(200);
-        // //$this->assertResponseRedirects('vacances/calendrier');
-        
-        // dd($client);
-
     public function testConnexion(): void
     {
         $client = static::createClient();
+        $doctrine = $client->getContainer()->get('doctrine');
+
+        static $metadata = null;
+
+        if ($metadata === null) {
+            $metadata = $doctrine->getManager()->getMetadataFactory()->getAllMetadata();
+        }
+
+        $schemaTool = new SchemaTool($doctrine->getManager());
+        $schemaTool->dropDatabase();
+
+        if (!empty($metadata)) {
+            $schemaTool->createSchema($metadata);
+        }
+        
+
         $user = (new User())
             ->setUsername('rchardon')
             ->setRoles(['ROLE_ADMIN'])
-            ->setPassword('$argon2id$v=19$m=65536,t=4,p=1$hZpxC3CwbyHkj4Ldqdwx7A$j9khFofLQYiCRcKULBqYy4Bp+PCSJ5CyTmnC1nsfxqA')
+            ->setPassword('$argon2id$v=19$m=65536,t=4,p=1$p7qP/12IPAz543KO1yymmQ$VESelxr6bDGigUeOpbIGc7ydFJHUcVpCogModOZD4t8')
             ->setNom('Chadon')
             ->setPrenom('Romain')
-            ->setMail('rchardon@gmail.com')
-            ->setGroupe(new Groupe('dev', "#FFF"));
+            ->setMail('rchardon@gmail.com');
+            //->setGroupe(new Groupe('dev', "#FFF"));
+
+        $doctrine->getManager()->persist($user);
+        $doctrine->getManager()->flush();
+        
         
         $session = $client->getContainer()->get('session');
         $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
@@ -57,7 +62,7 @@ class CalendrierControllerTest extends WebTestCase
         $cookie = new Cookie($session->getName(), $session->getId());
         $client->getCookieJar()->set($cookie);
 
-        $client->request('GET', '/vacances/calendrier');
+        $client->request('GET', 'vacances/gestionGroupe/groupe');
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
         
     }
