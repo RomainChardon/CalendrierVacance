ac538521b37b4c59deb1211eada3a976e2753fed
RomainChardon <romain.chardon.sio2020@gmail.com>
2022-01-26 10:25:57 +0100

    test

--- migrations/Version20220126092445.php
+++ migrations/Version20220126092445.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220125091807 extends AbstractMigration
+final class Version20220126092445 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20220125091807 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE vacances CHANGE date_demande date_demande DATETIME DEFAULT NULL, CHANGE date_annulation date_annulation DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD cadre TINYINT(1) DEFAULT NULL, DROP desactiver');
+        $this->addSql('ALTER TABLE vacances ADD annuler TINYINT(1) DEFAULT NULL, ADD date_demande DATETIME DEFAULT NULL, ADD date_annulation DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE vacances CHANGE date_demande date_demande DATE DEFAULT NULL, CHANGE date_annulation date_annulation DATE DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD desactiver TINYINT(1) NOT NULL, DROP cadre');
+        $this->addSql('ALTER TABLE vacances DROP annuler, DROP date_demande, DROP date_annulation');
     }
 }
